065cdd4ceb16df84642c094a097b8fd69df5a083
leksyking <foogundipe@student.oauife.edu.ng>
2022-09-25 23:32:11 +0100

    add new files and welcome.go

--- go.mod
+++ go.mod
@@ -3,9 +3,16 @@ module github.com/leksyking/video-conferencing
 go 1.19
 
 require (
+	github.com/gofiber/fiber/v2 v2.37.1
+	github.com/gofiber/template v1.7.1
+	github.com/gofiber/websocket/v2 v2.0.25
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/gofiber/fiber/v2 v2.37.1 // indirect
+	github.com/fasthttp/websocket v1.5.0 // indirect
 	github.com/klauspost/compress v1.15.10 // indirect
+	github.com/savsgio/gotils v0.0.0-20211223103454-d0aaa54c5899 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,7 +13,7 @@ import (
 )
 
 var (
-	addr = flag.String("addr,":"", os.Getenv("PORT"), "")
+	addr = flag.String("addr: ", os.Getenv("PORT"), "")
 	cert = flag.String("cert", "", "")
 	key  = flag.String("key", "", "")
 )
@@ -20,20 +21,28 @@ var (
 func Run() error {
 	flag.Parse()
 
-	if *addr == ":"{
+	if *addr == ":" {
 		*addr = ":8080 "
 	}
-	app := fiber.App()
+
+	engine := html.New("../views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
 
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket",)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
 	app.Get("/stream/:ssuid", handlers.Stream)
-	app.Get("/stream/:ssuid/websocket", )
-	app.Get("/stream/:ssuid/chat/websocket", )
-	app.Get("/stream/:ssuid/viewer/websocket",)
+	app.Get("/stream/:ssuid/websocket")
+	app.Get("/stream/:ssuid/chat/websocket")
+	app.Get("/stream/:ssuid/viewer/websocket")
+
+	return nil
 }
