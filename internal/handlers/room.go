1a3a800007c932f9a85076390e70bc9dd7f54166
leksyking <foogundipe@student.oauife.edu.ng>
2022-09-30 00:16:15 +0100

    add room create, room and websocket functions

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -4,9 +4,33 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
 	guuid "github.com/google/uuid"
 )
 
 func RoomCreate(c *fiber.Ctx) error {
-	return c.Redirect(fmt.Sprintf("/room/%s"), guuid.New().String())
+	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
+}
+
+func RoomWebsocket(c *websocket.Conn) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return nil
+	}
+	_, _, room := createOrGetRoom(uuid)
+	//room connection
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+}
+
+func createOrGetRoom(uuid string) (string, string, Room) {
+
 }
