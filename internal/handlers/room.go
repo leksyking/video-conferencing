41b5a9a71b208d8b2d90b868699e4f58b488fadd
leksyking <foogundipe@student.oauife.edu.ng>
2023-01-13 11:10:57 +0100

    add the createorget room and room viewer connection handlers

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"os"
+	"time"
 
+	"video-conferencing/pkg/chat"
 	w "video-conferencing/pkg/webrtc"
 
 	"github.com/gofiber/fiber/v2"
@@ -49,7 +52,32 @@ func Room(c *fiber.Ctx) error {
 }
 
 func createOrGetRoom(uuid string) (string, string, *w.Room) {
+	w.RoomsLock.Lock()
+	defer w.RoomsLock.Unlock()
 
+	h := sha256.New()
+	h.Write([]byte(uuid))
+	suuid := fmt.Sprintf("%x", h.Sum(nil))
+
+	if room := w.Rooms[uuid]; room != nil {
+		if _, ok := w.Streams[suuid]; !ok {
+			w.Streams[suuid] = room
+		}
+		return uuid, suuid, room
+	}
+
+	hub := chat.NewHub()
+	p := &w.Peers{}
+	p.TrackLocals = make(map[string]*webrtc.TrackLocalStaticRTP)
+	room := &w.Room{
+		Peers: p,
+		Hub:   hub,
+	}
+
+	w.Rooms[uuid] = room
+	w.Streams[suuid] = room
+	go hub.Run()
+	return uuid, suuid, room
 }
 
 func RoomViewerWebsocket(c *websocket.Conn) {
@@ -65,8 +93,22 @@ func RoomViewerWebsocket(c *websocket.Conn) {
 	}
 	w.RoomsLock.Unlock()
 }
-func roomViewerConn(c *websocket.Conn, p *w.Peers) {
 
+func roomViewerConn(c *websocket.Conn, p *w.Peers) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
+
+	for {
+		select {
+		case <-ticker.C:
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
 }
 
 type websocketMessage struct {
