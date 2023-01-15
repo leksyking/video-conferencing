0dff393b25701df1dd7d2465b2d09a3f62572f45
leksyking <foogundipe@student.oauife.edu.ng>
2023-01-15 19:59:57 +0100

    add the viewer connection function

--- internal/handlers/stream.go
+++ internal/handlers/stream.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"os"
+	"time"
 	w "video-conferencing/pkg/webrtc"
 
 	"github.com/gofiber/fiber/v2"
@@ -65,5 +66,17 @@ func StreamViewerWebSocket(c *websocket.Conn) {
 }
 
 func ViewerConn(c *websocket.Conn, p *w.Peers) {
-
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
+	for {
+		select {
+		case <-ticker.C:
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
 }
