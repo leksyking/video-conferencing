a4c08117790b72e6ce3141a9e48c488fabe6a82f
leksyking <foogundipe@student.oauife.edu.ng>
2023-01-25 14:38:59 +0100

    add functions skeleton for peers.go

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -3,6 +3,8 @@ package webrtc
 import (
 	"sync"
 	"video-conferencing/pkg/chat"
+
+	"github.com/gofiber/websocket/v2"
 )
 
 type Room struct {
@@ -14,7 +16,37 @@ type Peers struct {
 	Connections []PeerConnectionState
 	TrackLocals map[string]*webrtc.TrackLocalStaticRTP
 }
+type PeerConnectionState struct {
+	PeerConnection *webrtc.PeerConnection
+	websocket      *ThreadSafeWriter
+}
+type ThreadSafeWriter struct {
+	Conn  *websocket.Conn
+	Mutex sync.Mutex
+}
+
+func (t *ThreadSafeWriter) WriteJSON(v interface{}) error {
+	t.Mutex.Lock()
+	defer t.Mutex.Unlock()
+	return t.Conn.WriteJSON(v)
+}
+
+func (p *Peers) AddTrack(t *webrtc.TrackRemote) *webrtc.TrackLocalStaticRTP {
+
+}
+
+func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP) {
+
+}
+
+func (p *Peers) SignalPeerConnection() {
+
+}
 
 func (p *Peers) DispatchKeyFrame() {
+}
 
+type websocketMessage struct {
+	Event string `json: "event"`
+	Data  string `json:"data"`
 }
