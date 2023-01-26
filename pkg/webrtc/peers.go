530aefccbaf6291e991ef0d9d7da641537b1a0d0
leksyking <foogundipe@student.oauife.edu.ng>
2023-01-27 00:20:30 +0100

    add the codes for the functions of peers.go

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -1,6 +1,7 @@
 package webrtc
 
 import (
+	"log"
 	"sync"
 	"video-conferencing/pkg/chat"
 
@@ -32,15 +33,72 @@ func (t *ThreadSafeWriter) WriteJSON(v interface{}) error {
 }
 
 func (p *Peers) AddTrack(t *webrtc.TrackRemote) *webrtc.TrackLocalStaticRTP {
+	p.ListLock.Lock()
+	defer func() {
+		p.ListLock.Unlock()
+		p.SignalPeerConnections()
+	}()
+
+	trackLocal, err := webrtc.NewTrackLocalStaticRTP(t.Codec().RTPCodelcCapability, t.ID(), t.SreamID())
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	p.TrackLocals[t.ID()] = trackLocal
+	return (*invalid type)(&trackLocal)
 
 }
 
 func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP) {
-
+	p.ListLock.Lock()
+	defer func (){
+		p.ListLock.Unlock()
+		p.SignalPeerConnections()
+	}()
+	delete(p.TrackLocals, t.ID())
 }
 
-func (p *Peers) SignalPeerConnection() {
+func (p *Peers) SignalPeerConnections() {
+	p.ListLock.Lock()
+	defer func(){
+		p.ListLock.Unlock()
+		p.DispatchKeyFrame()
+	}()
 
+	attemptSync := func() (tryAgain bool) {
+		for i := range p.Connections{
+			if p.Connections[i].PeerConnection.ConnectionState() == webrtc.PeerConnectionStateClosed{
+				p.Connections = append(p.Connections[:i], p.Connections[i+1:]...)
+				log.Println("a", p.Connections)
+				return true
+			}
+			existingSenders := map[string]bool
+			for _, sender := range p.Connections[i].PeerConnection.GetSenders(){
+				if sender.Track() == nil{
+					continue
+				}
+				existingSenders[sender.Track().ID()] = true
+				if _, ok := p.TrackLocals[sender.Track().ID()]; !ok {
+					if err := p.Connections[i].PeerConnection.RemoveTrack(sender); err != nil {
+						return true
+					}
+				}
+			}
+			for _, receiver := range p.Connections[i].PeerConnection.GetReceivers(){
+				if receiver.Track() == nil{
+					continue
+				}
+				existingSenders[receiver.Track().ID()] == true
+			}
+			for trackID := range p.TrackLocals{
+				if _, ok := existingSenders[trackID]; !ok{
+					if _, err := p.Connections[i].PeerConnection.AddTrack(p.TrackLocals[trackID]): err != nil {
+						return true
+					}
+				}
+			}
+		}
+	}
 }
 
 func (p *Peers) DispatchKeyFrame() {
